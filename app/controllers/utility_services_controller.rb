acdabe6c7023edfb05e1cef9e85d9468306e2ed8
Richard Roepke <RAERoepke@gmail.com>
2018-07-17 14:39:59 -0400

    Initial utility page functionality
    Added in the ability to enter data, have it validated, and show (presumably) the valid xml request.

--- app/controllers/utility_services_controller.rb
+++ app/controllers/utility_services_controller.rb
@@ -1,15 +1,63 @@
+require 'builder'
+
 class UtilityServicesController < ApplicationController
   def generic
-    if params[:info]
-      puts "It works!"
+    if params[:utility_form]
       @request = params[:utility_form][:requestID].to_s
       @park = params[:utility_form][:parkID].to_s
       @security = params[:utility_form][:securityKey].to_s
-    else 
+    else
       @request = "UnitTypeInfoRequest" 
       @park = ""
       @security = ""
     end
+    
+    if params[:info]
+      if params[:info] == "Check XML"
+        @xmlshow = true
+      else
+        @xmlshow = false
+      end
+      
+      if params[:info] == "Check XML" || params[:info] == "Submit"
+        request_valid = UtilityValidator.new
+        request_valid.requestID = @request
+        request_valid.parkID = @park
+        request_valid.securityKey = @security
+        
+        @request_xml = buildXML if request_valid.valid?
+        
+        unless request_valid.valid?
+          @errors = request_valid.errors
+        end
+      else
+        @request_xml = nil
+      end
+      
+      if params[:info] == "Submit" && @request_xml.present?
+        puts "Yep."
+      end
+    end
+  end
+
+  def buildXML
+    xml = Builder::XmlMarkup.new(:target => @request_xml, :indent=>2)
+    xml.instruct! :xml, :version=>"1.0" #:content_type=>"text/xml" #, :encoding=>"UTF-8"
+    xml.tag!("Envelope", "xmlns:xsi"=>'http://www.w3.org/2001/XMLSchema-instance', "xsi:noNamespaceSchemaLocation"=>'file:/home/bys/Desktop/SHARE/xml2/SiteTypeInfoRequest/siteTypeInfoRequest.xsd')  {
+      xml.tag!("Body") {
+        xml.tag!(@request.chomp("Request").downcase){
+          xml.tag!("RequestData"){
+            xml.tag!("RequestIdentification"){
+              xml.ServiceRequestID @request
+              xml.tag!("CampGroundIdentification"){
+                xml.CampGroundUserName @park
+                xml.CampGroundSecurityKey @security
+              }
+            }
+          }
+        }
+      }
+    }
   end
 
 end
--- app/models/utility_validator.rb
+++ app/models/utility_validator.rb
@@ -6,7 +6,7 @@ class UtilityValidator
   attr_accessor :securityKey
 
   validates :requestID, inclusion: { in: %w{UnitTypeInfoRequest SiteTypeInfoRequest NotesAndTermsRequest}}
-  validates :parkID, length: { is: 6}
-  validates :parkID, format: { with: /\A^M.*\z/, message: "CampGroundUserName must start with a M" }
-  validates :securityKey, presence: true
+  validates :parkID, length: { is: 6, message: "Camp Ground User Name must be 6 characters"}
+  validates :parkID, format: { with: /\A^M.*\z/, message: "Camp Ground User Name must start with a M" }
+  validates :securityKey, presence: { message: "Security Key is required" }
 end
