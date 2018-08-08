bd6fac0e815d98bc79af7109e469917fa2e99f1d
Richard Roepke <RAERoepke@gmail.com>
2018-08-08 14:04:30 -0400

    Add Letter_Opener for Email Checking

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Deliever to letter opener instead of the actual email address.
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
